cbf31b67d022509d0212823938f014dae2c6d626
NanTarou <narutaka@minamide.org>
2025-11-20 16:33:17 +0900

    AddFile

---
+++ vbc_chip.pl
@@ -0,0 +1,92 @@
+# !/usr/bin/perl
+
+#use utf8;	//内部文字にはしない
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $Command  = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe";
+my $CNECTNIC = "NIC 2";
+my $NETNAME  = "VirtualBox Host-Only Ethernet Adapter";
+
+my @l_VPCLIST = ();
+my %h_VPCUID  = ();
+my %h_Nic2Vpc = ();
+my %h_Nic2IP  = ();
+
+##VPC起動リスト
+my $VL = `"$Command" list runningvms`;
+if($VL eq "")
+{
+	print "起動していない";
+	exit;
+}
+
+##
+my @VLS = split(/\n/, $VL);
+foreach (@VLS)
+{
+	if ($_ =~ /"(.*)" \{(.*)\}/) 
+	{
+		my $vpcname = $1;
+		push(@l_VPCLIST, $1);
+		$h_VPCUID{$1} = $2;
+		
+		my $NICL = `"$Command" showvminfo --details $2`;
+		
+		my @NICLS = split(/\n/, $NICL);
+		foreach (@NICLS)
+		{
+			if ($_ =~ /^$CNECTNIC:(.*)/) 
+			{
+				my $NICD = $1;
+				my @NICDS = split(/,/, $NICD);
+				foreach (@NICDS)
+				{
+					if ($_ =~ /MAC:(.*)/) 
+					{
+						my $tmp = $1;
+						$tmp =~ s/^\s+//;
+						$h_Nic2Vpc{$vpcname} = $tmp;
+					}
+				}
+			}
+		}
+	}
+}
+
+##
+foreach (@l_VPCLIST)
+{
+	my $vpcname = $_;
+	my $NPCIP = `"$Command" dhcpserver findlease --interface "$NETNAME" --mac-address=$h_Nic2Vpc{$vpcname}`;
+	my @NPCIPLS = split(/\n/, $NPCIP);
+	foreach (@NPCIPLS)
+	{
+		if ($_ =~ /^IP Address:(.*)/) 
+		{
+			my $tmp = $1;
+			$tmp =~ s/^\s+//;
+			$h_Nic2IP{$vpcname} = $tmp;
+		}
+	}
+}
+
+##
+my $nomber = 1;
+foreach (@l_VPCLIST)
+{
+	my $ip = $h_Nic2IP{$_};
+	if( $nomber < 10 )
+	{
+		print sprintf("  %d  %s  %s \n",$nomber,$ip,$_);
+	}else{
+		print sprintf(" %d  %s  %s \n",$nomber,$ip,$_);	#100からはズレるよw
+	}
+	++$nomber;
+}
+
+
+
+
+
---
+++ vpc_up.pl
@@ -0,0 +1,107 @@
+# !/usr/bin/perl
+
+#use utf8;	//内部文字にはしない
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $Command  = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe";
+#my $CNECTNIC = "NIC 2";
+#my $NETNAME  = "VirtualBox Host-Only Ethernet Adapter";
+
+##コンソールをUTF8に
+my $COMMND1 = `chcp 65001`;
+
+print "NanTarou VirtualBox Manage\n\n";
+
+my @l_VPCLIST = ();
+my %h_VPCUID  = ();
+my %h_VPCMOVE = ();
+my %h_No2Vpc  = ();
+
+##VPCリスト
+my $VL1 = `"$Command" list vms`;
+if($VL1 eq "")
+{
+	print "VPCが存在しません。";
+	exit;
+}
+my @VLS1 = split(/\n/, $VL1);
+foreach (@VLS1)
+{
+	if ($_ =~ /"(.*)" \{(.*)\}/) 
+	{
+		push(@l_VPCLIST, $1);
+		$h_VPCUID{$1} = $2;
+		$h_VPCUID{$1} = "0";
+	}
+}
+
+##VPC起動確認
+my $VL2 = `"$Command" list runningvms`;
+if($VL2 eq "")
+{
+	#print "VPCが起動していません。";
+	#exit;
+}
+my @VLS2 = split(/\n/, $VL2);
+foreach (@VLS2)
+{
+	if ($_ =~ /"(.*)" \{(.*)\}/) 
+	{
+		$h_VPCUID{$1} = "1";
+	}
+}
+
+##
+my $nomber = 1;
+foreach (@l_VPCLIST)
+{
+	my $msgmove = "   ";
+	if( $h_VPCUID{$_} ){ $msgmove = "run"; }
+	if( $nomber < 10 )
+	{
+		print sprintf("  %d %s %s \n",$nomber,$msgmove,$_);
+	}else{
+		print sprintf(" %d %s %s \n",$nomber,$msgmove,$_);	#100からはズレるよw
+	}
+	$h_No2Vpc{$nomber} = $_;
+	++$nomber;
+}
+
+##
+print "\n起動する番号を入力してください。\n";
+my $line = <STDIN>;
+chomp($line);
+
+##
+my $wupname = $h_No2Vpc{$line};
+if (defined($wupname))
+{
+	if( $wupname eq "" )
+	{
+		print "終了します。\n";
+		exit;
+	}
+}else{
+	print "終了します。\n";
+	exit;
+}
+
+if( $h_VPCUID{$h_No2Vpc{$line}} )
+{
+	print "すでに起動しています。\n";
+	exit;
+}
+
+print "$wupname を起動しています。\n";
+my $VL3 = `"$Command" startvm "$wupname" --type headless`;
+if($VL3 eq "")
+{
+	print "$wupnameの起動に失敗しました。";
+	exit;
+}
+
+print "起動しました。システムが起動するまでしばらくお待ちください。\n";
+print "終了します。\n";
+exit;
